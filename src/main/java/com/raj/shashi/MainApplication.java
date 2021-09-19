a452de3587042d83dfee3b3f230c2ebf77109356
Shashi Raj <shraj@groupon.com>
2021-09-19 11:27:27 +0530

    ac

--- src/main/java/com/raj/shashi/MainApplication.java
+++ src/main/java/com/raj/shashi/MainApplication.java
@@ -5,7 +5,6 @@ import com.raj.shashi.query.QueryExecutor;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class MainApplication {
 
